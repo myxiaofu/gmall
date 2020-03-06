a1110b43d2f493436e274165668173b07b847eae
xf <15455206@qq.com>
2020-03-06 23:38:01 +0800

    比较完整

---
+++ gmall-sms/src/main/java/com/atguigu/gmall/sms/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.atguigu.gmall.sms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author: xiaofu
+ * @DESCRIPTION: a technology seeker
+ * @date: 2020-03-06 21:13
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+       http.authorizeRequests().antMatchers("/**").permitAll();
+       http.csrf().disable();
+    }
+}
