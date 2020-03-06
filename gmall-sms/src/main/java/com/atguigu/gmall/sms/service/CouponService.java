cdf84f2be3b444a6883fccd8161947dff17158f6
xf <15455206@qq.com>
2020-03-06 23:22:53 +0800

    修改后

---
+++ gmall-sms/src/main/java/com/atguigu/gmall/sms/service/CouponService.java
@@ -0,0 +1,20 @@
+package com.atguigu.gmall.sms.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.atguigu.gmall.sms.entity.CouponEntity;
+import com.atguigu.core.bean.PageVo;
+import com.atguigu.core.bean.QueryCondition;
+
+
+/**
+ * 优惠券信息
+ *
+ * @author lixianfeng
+ * @email lxf@atguigu.com
+ * @date 2020-03-06 22:23:35
+ */
+public interface CouponService extends IService<CouponEntity> {
+
+    PageVo queryPage(QueryCondition params);
+}
+
