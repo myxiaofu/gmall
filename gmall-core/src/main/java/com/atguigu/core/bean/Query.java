99eb009695670eac28e4543c37a3c2a01c809ef4
xf <15455206@qq.com>
2020-03-06 22:32:28 +0800

    项目基本完成

--- gmall-core/src/main/java/com/atguigu/core/bean/Query.java
+++ gmall-core/src/main/java/com/atguigu/core/bean/Query.java
@@ -1,11 +1,3 @@
-/**
- * Copyright (c) 2016-2019 谷粒开源 All rights reserved.
- *
- * https://www.guli.cloud
- *
- * 版权所有，侵权必究！
- */
-
 package com.atguigu.core.bean;
 
 import com.atguigu.core.utils.SQLFilter;
